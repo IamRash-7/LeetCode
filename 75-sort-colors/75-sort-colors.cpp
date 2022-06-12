e426d3da34eb90a2d2c43b79d29f206bbc455d0f
Rasswanth <61219215+IamRash-7@users.noreply.github.com>
2022-06-12 10:58:51 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (24.36%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zer=0, one=0, two=0;
-        int j;
-        for(int i:nums)
+        int low = 0, mid=0,high=nums.size()-1;
+        while(mid<=high)
         {
-            if(i==0)
-                zer++;
-            else if(i==1)
-                one++;
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+                mid++;
             else
-                two++;
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
-        for(j=0;j<zer;j++)
-            nums[j]=0;
-        for(;j<zer+one;j++)
-            nums[j]=1;
-        for(;j<zer+one+two;j++)
-            nums[j]=2;
+        
+        
+        
+        // int zer=0, one=0, two=0;
+        // int j;
+        // for(int i:nums)
+        // {
+        //     if(i==0)
+        //         zer++;
+        //     else if(i==1)
+        //         one++;
+        //     else
+        //         two++;
+        // }
+        // for(j=0;j<zer;j++)
+        //     nums[j]=0;
+        // for(;j<zer+one;j++)
+        //     nums[j]=1;
+        // for(;j<zer+one+two;j++)
+        //     nums[j]=2;
     }
 };
