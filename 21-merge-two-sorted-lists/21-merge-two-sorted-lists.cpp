13340884a4edd25a96b52a0e02e3af6b8e728132
Rasswanth <61219215+IamRash-7@users.noreply.github.com>
2022-06-16 09:52:10 +0530

    Time: 20 ms (10.21%), Space: 14.9 MB (42.89%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,56 +11,27 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL || list2==NULL)
-        {
-            if(list1==NULL)
-                return list2;
-            else if(list2==NULL)
-                return list1;
-            else
-                return NULL;
-        }
-        int maxi=0;
-        if(list1->val <= list2->val)
-        {
-            maxi = list1->val;
-            list1 = list1->next;
-        }
-        else
-        {
-            maxi = list2->val;
-            list2 = list2->next;
-
-        }
-        ListNode *ptr = new ListNode(maxi);
-        ListNode *res = ptr;
+        if(list1==NULL)
+            return list2;
+        if(list2==NULL)
+            return list1;
+        
+        if(list1->val > list2->val)
+            swap(list1,list2);
+        
+        ListNode *res = list1;
         while(list1!=NULL && list2!=NULL)
         {
-            if(list1->val <= list2->val)
+            ListNode *tmp = NULL;
+            while(list1!=NULL && list1->val <= list2->val)
             {
-                ptr->next = new ListNode(list1->val);
+                tmp = list1;
                 list1 = list1->next;
             }
-            else
-            {
-                ptr->next = new ListNode(list2->val);
-                list2 = list2->next;
-            }
-            ptr = ptr->next;
+            tmp->next = list2;
+            swap(list1,list2);
         }
-        while(list1!=NULL)
-        {
-            ptr->next = new ListNode(list1->val);
-            list1 = list1->next;
-            ptr = ptr->next;
-        }
-        while(list2!=NULL)
-        {
-            ptr->next = new ListNode(list2->val);
-            list2 = list2->next;
-            ptr = ptr->next;
-        }
-        // list1->next = list2;
         return res;
+        
     }
 };
