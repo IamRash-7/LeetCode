fb295a0492d5c46329a723b6e90ddf5c7dfd1a2a
Rasswanth <61219215+IamRash-7@users.noreply.github.com>
2022-06-16 09:33:01 +0530

    Time: 23 ms (5.00%), Space: 15.1 MB (8.29%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,43 +11,56 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // cnt->next = list1;
-        // while(list1!=NULL && list2!=NULL)
-        // {
-        //     if(list1->val >= list2->val)
-        //     {
-        //         list1 = list1->next;
-        //         cnt = cnt->next;
-        //     }
-        //     else
-        //     {
-        //         cnt->next = list2;
-        //         ListNode* tmp = list2->next;
-        //         list2->next = list1;
-        //         list2 = tmp;
-        //     }
-        // }
-        // return list1;
-        ListNode tail(INT_MIN); 
-        ListNode* cnt = &tail;
-        while(list1 && list2)
+        if(list1==NULL || list2==NULL)
         {
-            if(list1->val >= list2->val)
+            if(list1==NULL)
+                return list2;
+            else if(list2==NULL)
+                return list1;
+            else
+                return NULL;
+        }
+        int maxi=0;
+        if(list1->val <= list2->val)
+        {
+            maxi = list1->val;
+            list1 = list1->next;
+        }
+        else
+        {
+            maxi = list2->val;
+            list2 = list2->next;
+
+        }
+        ListNode *ptr = new ListNode(maxi);
+        ListNode *res = ptr;
+        while(list1!=NULL && list2!=NULL)
+        {
+            if(list1->val <= list2->val)
             {
-                cnt->next = list2;
-                list2 = list2->next;
+                ptr->next = new ListNode(list1->val);
+                list1 = list1->next;
             }
             else
             {
-                cnt->next = list1;
-                list1 = list1->next;
+                ptr->next = new ListNode(list2->val);
+                list2 = list2->next;
             }
-            cnt = cnt->next;
+            ptr = ptr->next;
         }
-        if(list1)
-            cnt->next = list1;     
-        else
-            cnt->next = list2; 
-        return tail.next;
+        while(list1!=NULL)
+        {
+            ptr->next = new ListNode(list1->val);
+            list1 = list1->next;
+            ptr = ptr->next;
+        }
+        while(list2!=NULL)
+        {
+            ptr->next = new ListNode(list2->val);
+            list2 = list2->next;
+            ptr = ptr->next;
+        }
+        // list1->next = list2;
+        return res;
     }
 };
