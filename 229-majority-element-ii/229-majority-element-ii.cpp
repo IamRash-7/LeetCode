c320b628dc6b0f38df05b7692967310ca223c11f
Rasswanth <61219215+IamRash-7@users.noreply.github.com>
2022-07-21 09:50:58 +0530

    Time: 15 ms (82.14%), Space: 15.8 MB (51.28%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n1 = -1, n2 = -1, c1=0, c2=0;
+        int n1 = -1, n2 = -1, c1 = 0, c2 = 0, n=nums.size();
+        vector<int> ans;
         for(int i:nums)
         {
             if(i==n1)
@@ -10,8 +11,8 @@ public:
                 c2++;
             else if(c1==0)
             {
-                n1 = i;
-                c1=1;
+                n1=i;
+                c1 = 1;
             }
             else if(c2==0)
             {
@@ -24,8 +25,7 @@ public:
                 c2--;
             }
         }
-        vector<int> ans;
-        c1 = c2 = 0;
+        c1 = 0, c2 = 0;
         for(int i:nums)
         {
             if(i==n1)
@@ -33,10 +33,12 @@ public:
             else if(i==n2)
                 c2++;
         }
-        if(c1>nums.size()/3)
+        if(c1>n/3)
             ans.push_back(n1);
-        if(c2>nums.size()/3)
+        if(c2>n/3)
             ans.push_back(n2);
+        
         return ans;
     }
+    
 };
